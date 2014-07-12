4596d64dc1008bbe7ee16420daa5323c03d34f00
Garris0n- <garrison.cgpvp@gmail.com>
2014-07-12 10:03:58 -0400

    Overly descriptive commit message.

--- src/main/java/com/tenjava/entries/Garris0n-/t3/TenJava.java
+++
@@ -1,5 +0,0 @@
-package com.tenjava.entries.Garris0n_.t3;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class TenJava extends JavaPlugin {}
---
+++ src/main/java/nu/gar/tenjava/TenJavaPlugin.java
@@ -0,0 +1,8 @@
+package nu.gar.tenjava;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TenJavaPlugin extends JavaPlugin{
+
+
+}
