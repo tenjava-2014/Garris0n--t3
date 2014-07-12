5ff791040c9858c0a2e5b307950372b6f5e62c52
Garris0n- <garrison.cgpvp@gmail.com>
2014-07-12 10:40:30 -0400

    How many javas do you have?  I have ten.

---
+++ src/main/java/nu/gar/plague/Main.java
@@ -0,0 +1,23 @@
+package nu.gar.plague;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+    @Override
+    public void onEnable(){
+
+        //ohai
+
+    }
+
+    @Override
+    public void onDisable(){
+
+        //kbye
+
+    }
+
+
+
+}
--- src/main/java/nu/gar/tenjava/TenJavaPlugin.java
+++
@@ -1,8 +0,0 @@
-package nu.gar.tenjava;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class TenJavaPlugin extends JavaPlugin{
-
-
-}
